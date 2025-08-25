e856cf8a7427e493863092b8dc89875adbcb3ca3
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-08-25 20:51:36 -0300

    integration in auth context

--- dt-money/src/context/auth.context.tsx
+++ dt-money/src/context/auth.context.tsx
@@ -1,10 +1,14 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 
+import * as authService from "@/shared/services/dt-money/auth.service";
+
+import type { IUser } from "@/shared/interfaces/user-interface";
+
 import type { LoginFormParams } from "@/screens/login/login-form";
 import type { RegisterFormParams } from "@/screens/register/register-form";
 
 type AuthContextType = {
-  user: null;
+  user: IUser | null;
   token: string | null;
   handleAuthenticate: (params: LoginFormParams) => Promise<void>;
   handleRegister: (params: RegisterFormParams) => Promise<void>;
@@ -20,10 +24,15 @@ type AuthContextProviderProps = {
 };
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
+  const [user, setUser] = useState<IUser | null>(null);
+  const [token, setToken] = useState<string | null>(null);
 
-  async function handleAuthenticate({ email, password }: LoginFormParams) {}
+  async function handleAuthenticate(userData: LoginFormParams) {
+    const { user, token } = await authService.authenticate(userData);
+
+    setUser(user);
+    setToken(token);
+  }
 
   async function handleRegister(formData: RegisterFormParams) {}
 
