554e5e9d8ddeafaee9b36b16fe78a5ace9f60664
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-08-26 18:54:42 -0300

    creating the restoresession function

--- dt-money/src/context/auth.context.tsx
+++ dt-money/src/context/auth.context.tsx
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createContext, ReactNode, useContext, useState } from "react";
 
 import * as authService from "@/shared/services/dt-money/auth.service";
@@ -6,6 +7,7 @@ import type { IUser } from "@/shared/interfaces/user-interface";
 
 import type { LoginFormParams } from "@/screens/login/login-form";
 import type { RegisterFormParams } from "@/screens/register/register-form";
+import type { IAuthenticateResponse } from "@/shared/interfaces/https/authenticate-response";
 
 type AuthContextType = {
   user: IUser | null;
@@ -13,6 +15,7 @@ type AuthContextType = {
   handleAuthenticate: (params: LoginFormParams) => Promise<void>;
   handleRegister: (params: RegisterFormParams) => Promise<void>;
   handleLogout: () => void;
+  restoreUserSession: () => Promise<string | null>;
 };
 
 export const AuthContext = createContext<AuthContextType>(
@@ -32,6 +35,11 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
     setUser(user);
     setToken(token);
+
+    await AsyncStorage.setItem(
+      "dt-money-user",
+      JSON.stringify({ user, token }),
+    );
   }
 
   async function handleRegister(formData: RegisterFormParams) {
@@ -39,13 +47,38 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
     setUser(user);
     setToken(token);
+
+    await AsyncStorage.setItem(
+      "dt-money-user",
+      JSON.stringify({ user, token }),
+    );
   }
 
   function handleLogout() {}
 
+  async function restoreUserSession() {
+    const userData = await AsyncStorage.getItem("dt-money-user");
+
+    if (userData) {
+      const { user, token } = JSON.parse(userData) as IAuthenticateResponse;
+
+      setUser(user);
+      setToken(token);
+    }
+
+    return userData;
+  }
+
   return (
     <AuthContext.Provider
-      value={{ user, token, handleAuthenticate, handleRegister, handleLogout }}
+      value={{
+        user,
+        token,
+        handleAuthenticate,
+        handleRegister,
+        handleLogout,
+        restoreUserSession,
+      }}
     >
       {children}
     </AuthContext.Provider>
